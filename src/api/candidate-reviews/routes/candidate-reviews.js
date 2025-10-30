494b7e2e8194b2d6ff4de23737a67ba5a71290c4
UdoreDevDaddy <dev@devdaddy.co.za>
2025-10-30 08:00:26 +0200

    updated werkie

---
+++ src/api/candidate-reviews/routes/candidate-reviews.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * candidate-reviews router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::candidate-reviews.candidate-reviews');
---
+++ src/api/candidate-reviews/services/candidate-reviews.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * candidate-reviews service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::candidate-reviews.candidate-reviews');
